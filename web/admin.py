dabf0fcbdcc55aeaab26b0dd85c872d047074742
zzdnb <1955093734@qq.com>
2023-06-24 12:16:53 +0800

    add admin; update search input

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Note, NoteGroup
 
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'note_group', 'is_fast', 'user']
+    list_filter = ['user', 'note_group', 'is_fast']
+    search_fields = ['title', 'content']
+    ordering = ['id']
+
+
+class NoteGroupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user']
+    list_filter = ['user']
+    search_fields = ['name', 'user']
+    ordering = ['user']
+
+
 # 隐私不可见
-# admin.site.register(Note)
-# admin.site.register(NoteGroup)
+# admin.site.register(Note, NoteAdmin)
+# admin.site.register(NoteGroup, NoteGroupAdmin)
