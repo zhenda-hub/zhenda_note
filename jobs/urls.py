83e5910aa22d5ef6e6d18f39743d96ebb184f56f
ubuntu <ubuntu@localhost.localdomain>
2023-07-12 12:59:33 +0800

    update dbback

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,7 @@
 # print('here is urls')
 from django.urls import path
 
-from . import views
+# from . import views
 
 
 app_name = 'jobs'
--- utils/db_backup.py
+++ utils/db_backup.py
@@ -2,6 +2,11 @@ from datetime import datetime
 import sys
 from pathlib import Path
 
+
+PJ_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(PJ_DIR))
+
+
 from utils.path_manager import create_dir
 from utils.run_cmd import run_cmd2
 
@@ -11,7 +16,7 @@ def backup_db():
 
     create_dir('dbback')
     curtime = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    cmd = f'python manage.py dumpdata > dbback/db{curtime}.json --indent=4'
+    cmd = f'/www/server/pyporject_evn/94d7bd8179533c59aca987c718472328_venv/bin/python3 manage.py dumpdata > dbback/db{curtime}.json --indent=4'
 
     stdout, stderr = run_cmd2(cmd)
     print(stdout, stderr)
@@ -22,7 +27,6 @@ def print_hl():
 
 
 if __name__ == '__main__':
-    # backup_db()
-    PJ_DIR = Path(__file__).resolve().parent.parent
-    sys.path.append(str(PJ_DIR))
-    print_hl()
+    backup_db()
+    # print_hl()
+
